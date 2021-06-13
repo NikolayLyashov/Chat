68209e714d536840a5c3788d4036820831aa010e
ronin1991 <ronin1991@rambler.ru>
2021-06-13 19:58:34 +0300

    add gitignore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { Navigation } from '../components/Navigation';
 
 export const Home = () => {
+  useEffect(() => {});
   const token = localStorage.getItem('token');
-  axios.get('/api/v1/data', { Headers: { Authorization: `Bearer ${token}` } });
+  axios.get('/api/v1/data', { headers: { Authorization: `Bearer ${token}` } });
 
   return (
     <Navigation />
